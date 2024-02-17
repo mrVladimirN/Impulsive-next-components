8a54161c59a76202be02435e809d00c5844010ac
mrVladimirN <vladimir.nitu.business@outlook.com>
2024-02-17 11:38:24 +0200

    fix: build

--- src/firebase/server.ts
+++ src/firebase/server.ts
@@ -7,7 +7,24 @@ import { Auth } from 'firebase-admin/lib/auth/auth';
 
 let app: App | undefined;
 if (!admin.apps.length) {
-  app = admin.initializeApp({
+  app = process.env.FIREBASE_PRIVATE_KEY
+    ? admin.initializeApp({
+      credential: admin.credential.cert({
+        projectId: process.env.FIREBASE_PROJECT_ID
+          ? process.env.FIREBASE_PROJECT_ID
+          : 'fake',
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL
+          ? process.env.FIREBASE_CLIENT_EMAIL
+          : 'fake',
+        privateKey: process.env.FIREBASE_PRIVATE_KEY
+          ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')
+          : undefined
+      })
+    })
+    : undefined;
+}
+const adminDb: Firestore | undefined = process.env.FIREBASE_PRIVATE_KEY
+  ? initFirestore({
     credential: admin.credential.cert({
       projectId: process.env.FIREBASE_PROJECT_ID
         ? process.env.FIREBASE_PROJECT_ID
@@ -19,21 +36,8 @@ if (!admin.apps.length) {
         ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')
         : undefined
     })
-  });
-}
-const adminDb: Firestore = initFirestore({
-  credential: admin.credential.cert({
-    projectId: process.env.FIREBASE_PROJECT_ID
-      ? process.env.FIREBASE_PROJECT_ID
-      : 'fake',
-    clientEmail: process.env.FIREBASE_CLIENT_EMAIL
-      ? process.env.FIREBASE_CLIENT_EMAIL
-      : 'fake',
-    privateKey: process.env.FIREBASE_PRIVATE_KEY
-      ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')
-      : undefined
   })
-});
+  : undefined;
 const adminAuth: Auth = admin.auth(app);
 
 export { adminAuth, adminDb };
