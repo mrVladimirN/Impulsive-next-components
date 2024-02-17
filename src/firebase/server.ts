a9c8b6d52e307958b56b8cadcf7aaf00bb9fca5a
mrVladimirN <vladimir.nitu.business@outlook.com>
2024-02-17 11:33:31 +0200

    fix: build

--- src/firebase/server.ts
+++ src/firebase/server.ts
@@ -11,10 +11,10 @@ if (!admin.apps.length) {
     credential: admin.credential.cert({
       projectId: process.env.FIREBASE_PROJECT_ID
         ? process.env.FIREBASE_PROJECT_ID
-        : '',
+        : 'fake',
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL
         ? process.env.FIREBASE_CLIENT_EMAIL
-        : '',
+        : 'fake',
       privateKey: process.env.FIREBASE_PRIVATE_KEY
         ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')
         : undefined
@@ -25,10 +25,10 @@ const adminDb: Firestore = initFirestore({
   credential: admin.credential.cert({
     projectId: process.env.FIREBASE_PROJECT_ID
       ? process.env.FIREBASE_PROJECT_ID
-      : '',
+      : 'fake',
     clientEmail: process.env.FIREBASE_CLIENT_EMAIL
       ? process.env.FIREBASE_CLIENT_EMAIL
-      : '',
+      : 'fake',
     privateKey: process.env.FIREBASE_PRIVATE_KEY
       ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')
       : undefined
